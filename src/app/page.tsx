8a8648006dd34d1b2de424748dcd97d1d2f560b4
sushil2817 <tony281723@gmail.com>
2024-05-25 08:56:14 +0530

    tried to explore some new ui and add them into my project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return (
-      <h1 className="text-2xl text-center">Chai aur code</h1>
+    <h1 className="text-2xl text-center">Chai aur code</h1>
+    
   );
 }

2e1fbc1a28003c773d62ddb1b8e90e21b2564138
sushil2817 <tony281723@gmail.com>
2024-05-27 23:46:00 +0530

    add new feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 // import Image from "next/image"
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection"
+import WhyChooseUs from "../components/WhyChooseUs";
+
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
        <HeroSection /> 
        <FeaturedCourses></FeaturedCourses>
+         <WhyChooseUs></WhyChooseUs>
     </main>
   );
 }

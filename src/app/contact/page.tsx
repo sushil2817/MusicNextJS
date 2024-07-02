3366f18eb47f048f96dac2db9b08cb56bd0d25d5
sushil2817 <tony281723@gmail.com>
2024-07-02 23:22:21 +0530

    something cool

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { Meteors } from "@/components/ui/meteors"
 import { BackgroundBeams } from "@/components/ui/background-beams";
-import React, { FormEvent,useState } from "react";
+import React, { FormEvent, useState } from "react";
 
 
 function page() {
@@ -13,13 +13,13 @@ function page() {
     console.log('Submitted:', { email, message });
   };
   return (
-     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 py-12 pt-36 relative">
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 py-12 pt-36 relative">
       <BackgroundBeams />
       <Meteors number={100}></Meteors>
-         <div  className="max-w-2xl mx-auto p-4 relative z-10">
-               <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">Contact us</h1>
-               <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center">We're here to help with any questions about our courses, programs, or events. Reach out and let us know how we can assist you in your musical journey</p>
-               <form onSubmit={handleSubmit} className="space-y-4 mt-4">
+      <div className="max-w-2xl mx-auto p-4 relative z-10">
+        <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">Contact us</h1>
+        <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center">We're here to help with any questions about our courses, programs, or events. Reach out and let us know how we can assist you in your musical journey</p>
+        <form onSubmit={handleSubmit} className="space-y-4 mt-4">
           <input
             type="email"
             value={email}
@@ -43,10 +43,8 @@ function page() {
             Send Message
           </button>
         </form>
-         </div>
-         
-         
-     </div>
+      </div>
+    </div>
   )
 }
 
